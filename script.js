ac3f56c142c8f54d613a21bd602e6c394a1cec66
Hailey Rae <haileyraehughes89@gmail.com>
2023-04-11 17:57:29 -0500

    edits before quiz content function

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-var begin = document.getElementById("start");
 
+var output = document.getElementById("output");
+
+
+console.log("begin");
+
+var quizContent = [
+    {
+        text: "What color is the sky?",
+        choices: ["blue", "purple", "yellow", "green"],
+        answer: "blue"
+    },
+   {
+        text: "What color is the grass?",
+        choices: ["blue", "purple", "yellow", "green"],
+        answer: "green"
+   },
+   {
+    text: "What color is the sun?",
+    choices: ["blue", "purple", "yellow", "green"],
+    answer: "yellow"
+},
+ ];
+ var paragraph=document.createElement('h2');
+for (i=0; i<3; i++){ 
+
+var question = quizContent[i];
+var print = question['text'];
+paragraph.append(print);
+//paragraph.textContent = questions.text;
+};
+output.appendChild(paragraph);
