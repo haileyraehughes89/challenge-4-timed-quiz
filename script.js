dc801057986c922ab1d744b3b6ea9f80945fcdf8
Hailey Rae <haileyraehughes89@gmail.com>
2023-04-13 12:26:42 -0500

    successful addition of code that will score number of correct user responses.

--- script.js
+++ script.js
@@ -21,12 +21,13 @@ var quizContent = [ //defines a variable array that  contains all quiz questions
 
  var paragraph=document.getElementById("question"); 
  var answerOptions = document.getElementById("button"); 
-
+ var score = 0
 function quiz(assess){ 
     var question = assess.text  
         paragraph.textContent = question
     var selection = assess.choices
     var correctAnswer = assess.answer
+
         answerOptions.innerHTML = "" //this clears answerOptions element before generating new question
         selection.forEach((selection)=>{
        
@@ -36,20 +37,29 @@ function quiz(assess){
             btn.addEventListener("click",function move(){
                 var next = quizContent.indexOf(assess) + 1;
                 quiz(quizContent[next])  
+            // if (next > quizContent.length) {
+            //         console.log("Quiz complete! Final score");
+            //     }
             })
 
             btn.addEventListener ("click", function correct() {
                 rightAnswer = false
-                num = 0
+            
                 if (btn.textContent === correctAnswer) {
                     console.log("correct"), rightAnswer= true;
                 } else { console.log("incorrect")}
-                if (rightAnswer=true) {(num =+ 1), console.log()
+                
+                
+                if (rightAnswer) { score++; console.log("add point"
+                )
                 } else {console.log("no score change")}
+
+            console.log(score);
+                
             })
+           
         })
     }
-
 //answerOptions.addEventListener("click", handleClick);
 function showContent (){
      //this shows the very last question. I can change this to 0,1,2, 0+1, i+1 (goes back to end) maybe write a new function?
